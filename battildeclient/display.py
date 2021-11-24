ecc3f047e87185d5f7b230b985dc3a558330a6d8
troido <troido@protonmail.com>
2021-11-24 16:25:24 +0100

    removed field background

--- battildeclient/display.py
+++ battildeclient/display.py
@@ -35,7 +35,6 @@ class Display:
     
     def resizeField(self, size):
         self.getWidget("field").set_size(*size)
-        self.getWidget("fieldbackground").change()
     
     def drawFieldCells(self, cells):
         field = self.getWidget("field")
